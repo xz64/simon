2e6245826b751f07f78a26a667f840a1519e84b3
S K <xz64@users.noreply.github.com>
2015-11-30 19:13:38 -0500

    remove jest

--- __tests__/sum-test.js
+++
@@ -1,8 +0,0 @@
-import * as sum from '../app/sum'
-jest.dontMock('../app/sum')
-
-describe('sum', function() {
-  it('adds numbers', function() {
-    expect(sum(1,2)).toBe(3)
-  })
-})
--- app/sum.js
+++
@@ -1,3 +0,0 @@
-export default function(value1, value2) {
-  return value1 + value2
-}
