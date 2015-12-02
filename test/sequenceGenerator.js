86c65a070286a24ed6bbe2f34825d6c042f9c673
S K <xz64@users.noreply.github.com>
2015-12-01 23:30:19 -0500

    add sequenceGenerator tests

--- test/sequenceGenerator.js
+++ test/sequenceGenerator.js
@@ -1,10 +1,11 @@
 import test from 'tape';
 import sequenceGenerator from '../app/sequenceGenerator';
 
+/* Test length of returned sequence */
 function testLength(length) {
   return (assert) => {
-    let sequence3 = sequenceGenerator.generateSequence(length);
-    assert.equal(sequence3.length, length);
+    let sequence = sequenceGenerator.generateSequence(length);
+    assert.equal(sequence.length, length);
     assert.end();
   };
 }
@@ -15,4 +16,25 @@ let i;
 for(i = 0; i < length_tests.length; i++) {
   test('Returned sequence length is correct (length: ' + length_tests[i] + ')',
     testLength(length_tests[i]));
+}
+
+/* Test validity of items in each sequence */
+function inRange(x) {
+  return x >= 0 && x <= 3;
+}
+
+function testItems(length) {
+  return (assert) => {
+    let sequence = sequenceGenerator.generateSequence(length);
+    let i;
+    for(i = 0; i < sequence.length; i++) {
+      assert.assert(inRange(sequence[i]));
+    }
+    assert.end();
+  };
+}
+
+for(i = 0; i < length_tests.length; i++) {
+  test('Items between 0 & 3 (length: ' + length_tests[i] + ')',
+    testItems(length_tests[i]));
 }
