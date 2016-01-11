f6fbe3d70148bf9dee296a9db1c9cdb1b976e65b
S K <xz64@users.noreply.github.com>
2016-01-10 22:26:45 -0500

    can play 1 Sequence now

--- app/controller/QuadrantButtonController.js
+++ app/controller/QuadrantButtonController.js
@@ -5,6 +5,8 @@ export default class {
     this.emitter = new EventEmitter();
     this.quadrantButton.emitter.on('turnOn', this.turnOn, this);
     this.quadrantButton.emitter.on('turnOff', this.turnOff, this);
+    this.quadrantButton.emitter.on('lock', this.disableInput, this);
+    this.quadrantButton.emitter.on('unlock', this.enableInput, this);
     this.quadrantButtonView.emitter.on('onInput', this.onInput, this);
   }
 
@@ -18,5 +20,13 @@ export default class {
 
   onInput() {
     this.emitter.emit('input', this.quadrantButtonView.quadrant);
+  }
+
+  disableInput() {
+    this.quadrantButtonView.disableInput.call(this.quadrantButtonView);
+  }
+
+  enableInput() {
+    this.quadrantButtonView.enableInput.call(this.quadrantButtonView);
   }
 }
