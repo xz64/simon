f413b6a9ae6ad89999f495f33649cd6cc7f7ce17
S K <xz64@users.noreply.github.com>
2016-01-30 18:30:20 -0800

    make scoreboard positioning dynamic

--- app/view/Scoreboard.js
+++ app/view/Scoreboard.js
@@ -1,11 +1,14 @@
 /*global PIXI*/
 /*global EventEmitter*/
 
-export default class {
+import UIElement from './UIElement';
+
+export default class extends UIElement {
   constructor(width, height) {
-    this.width = width;
-    this.height = height;
+    super(width, height);
     this.emitter = new EventEmitter();
+    this.backgroundWidth = 110;
+    this.backgroundHeight = 24;
     this.graphicsContainer = new PIXI.Container();
     this.sprite = this.createSprite(this.createGraphics());
     this.label= this.createLabel();
@@ -15,36 +18,39 @@ export default class {
     this.graphicsContainer.addChild(this.label);
     this.graphicsContainer.addChild(this.score);
     this.graphicsContainer.addChild(this.scoreValue);
+    this.graphicsContainer.position.x = this.marginLeft + 0.5*this.boardSize - 
+      this.backgroundWidth / 2;
+    this.graphicsContainer.position.y = this.marginTop + 0.35*this.boardSize;
   }
 
   createGraphics() {
     let graphics = new PIXI.Graphics();
     graphics.beginFill(0x778899);
-    graphics.drawRoundedRect(0, 0, 110, 24, 5);
+    graphics.drawRoundedRect(0, 0, this.backgroundWidth, this.backgroundHeight,
+      10);
     graphics.endFill();
     return graphics;
   }
 
   createSprite(graphics) {
     let sprite = new PIXI.Sprite(graphics.generateTexture());
-    sprite.position.x = this.width*0.05;
-    sprite.position.y = this.height*0.05;
     return sprite;
   }
 
+  createText(string) {
+    return new PIXI.Text(string, {font: 'bold 20px Arial', align:'center',
+      color: '#FF0000'});
+  }
+
   createScore() {
-    let text = new PIXI.Text('Steps: ', {font: 'bold 20px Arial',
-      align:'center'});
-    text.position.x = this.width*0.05;
-    text.position.y = this.height*0.05;
+    let text = this.createText('Steps: ');
+    text.position.x = 5;
     return text;
   }
 
   createScoreValue() {
-    let text = new PIXI.Text('0', {font: 'bold 20px Arial',
-      align:'center'});
-    text.position.x = this.width*0.05 + 80;
-    text.position.y = this.height*0.05;
+    let text = this.createText('0');
+    text.position.x = this.backgroundWidth - 30;
     return text;
   }
 
